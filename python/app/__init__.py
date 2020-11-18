c77835cbd2c13b34468bcfd52ed9f98564a32b88
maxime-bc <maximeblanchon91490@gmail.com>
2020-11-18 20:34:12 +0100

    Add Bootstrap

--- python/app/__init__.py
+++ python/app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 
 from config import Config
@@ -8,6 +9,7 @@ app.config.from_object(Config)
 login_manager = LoginManager(app)
 login_manager.init_app(app)
 login_manager.login_view = 'login'
+bootstrap = Bootstrap(app)
 
 from app.models import db, User, Note
 
