c4a54cbf8fe0412f395a1a127d0136338807f35a
maxime-bc <maximeblanchon91490@gmail.com>
2020-11-24 12:11:23 +0100

    Fix docker-compose container names

--- python/config.py
+++ python/config.py
@@ -1,14 +1,10 @@
 class Config(object):
-    _project_name = 'no-sql-project'
-    _postgresql_host = _project_name + '_postgresql_1'
-    _redis_host = _project_name + '_redis_1'
-
     # flask config 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = '12345678'
 
     # postgresql config
-    SQLALCHEMY_DATABASE_URI = f'postgresql+psycopg2://docker:docker@{_postgresql_host}/docker'
+    SQLALCHEMY_DATABASE_URI = f'postgresql+psycopg2://docker:docker@postgresql/docker'
 
     # redis config
-    REDIS_URL = f'redis://:@{_redis_host}:6379/0'
+    REDIS_URL = f'redis://:@redis:6379/0'
